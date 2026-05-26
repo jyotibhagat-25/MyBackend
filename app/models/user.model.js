2672eb8a12b4cbcdc412c5e732ae7ebecd58d0e4
Jyoti Bhagat <jyotibhagat7079@gmail.com>
2026-05-26 08:59:39 +0530

    initial

---
+++ app/models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    username: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    roles: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role"
+      }
+    ]
+    ,
+    status: Boolean
+  })
+);
+
+module.exports = User;
